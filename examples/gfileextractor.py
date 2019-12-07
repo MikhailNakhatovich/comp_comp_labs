e0417820ac86a4f307020e1477612fe48a731d39
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-12-07 17:38:19 +0300

    Changed examples

--- examples/gfileextractor.py
+++ examples/gfileextractor.py
@@ -3,9 +3,9 @@ import numpy as np
 from gfileextractor import extract
 
 
-def run_example_extract():
+def run_example_extract(filename):
     mag_mesh = 65
-    flux, RBDRY, ZBDRY, NBDRY, R, Z, rdim, zdim = extract('g035685.00150', mag_mesh)
+    flux, RBDRY, ZBDRY, NBDRY, R, Z, rdim, zdim = extract(filename, mag_mesh)
     min = np.argmin(flux)
     rmin = min // mag_mesh
     zmin = min - rmin * mag_mesh
--- examples/grid.py
+++ examples/grid.py
@@ -3,7 +3,7 @@ from grid import generate_grid, draw_grid, generate_domain_grid, draw_domains
 
 
 def run_example_grid1():
-    border, center = run_example_extract()
+    border, center = run_example_extract('g035685.00150')
     lines, radial_lines = generate_grid(border, center, count=3, radials=6)
     draw_grid(lines, center, radial_lines)
     domains = generate_domain_grid(border, center, count=3, radials=6)
